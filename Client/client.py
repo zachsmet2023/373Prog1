30bb131a6495f2dc5a7922eb9520a7b789bc3069
Zach Smet <zach.smet@snc.edu>
2023-03-13 13:26:51 -0500

    recieves imgs from server

--- Client/client.py
+++ Client/client.py
@@ -1,29 +1,71 @@
 # UDPclient.py
-from socket import *  # include Python's socket library
+from socket import * 
 import sys
-
-if (len(sys.argv) != 4):
-    print('Usage: python client.py server_ip server_port get_text_file')
-    sys.exit()
-serverName = sys.argv[1]
-serverPort = int(sys.argv[2])
-fileName = sys.argv[3]
-
-# Create UDP socket for server
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-# Get user keyboard input
-message = input('What is your name?')
-
-# Attach server name, port to message; send into socket
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-
-# Read reply characters from socket into string
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-
-# Print out received string and close socket
-print(modifiedMessage.decode())
+from html.parser import HTMLParser
 
 
 
-clientSocket.close()
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.image_urls = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "img":
+            for name, value in attrs:
+                if name == "src":
+                    clientSocket.sendto(value.encode(), (serverName, serverPort))
+                    getIMG(clientSocket)
+
+                   
+
+def getHTML(clientSocket):
+  fileContents = ""
+  while 1:
+        recMessage, serverAddress = clientSocket.recvfrom(2048)
+        if (recMessage.decode() == "EOF"):
+            return fileContents
+        fileContents +=recMessage.decode()
+
+def getIMG(clientSocket):
+    recMessage, serverAddress = clientSocket.recvfrom(2048)
+    if (recMessage.decode() != "FILE NOT FOUND"):
+        print("IMAGE RECEIVED")
+    else:
+        print(recMessage.decode())
+
+
+
+if __name__ == '__main__':
+    # if (len(sys.argv) != 4):
+    #     print('Usage: python client.py server_ip server_port get_text_file')
+    #     sys.exit()
+    # serverName = sys.argv[1]
+    # serverPort = int(sys.argv[2])
+    # fileName = sys.argv[3]
+
+    serverName = '127.0.0.1'
+    serverPort = 12000
+    fileName = "index.html"
+    
+
+    # Create UDP socket for server
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    # Attach server name, port to message; send into socket
+    clientSocket.sendto(fileName.encode(), (serverName, serverPort))
+
+    # Retrieves all of the html file from server
+    HTMLFILE = getHTML(clientSocket)
+
+    #Creating Parser and giving it the html file
+    parser = MyHTMLParser()
+    parser.feed(HTMLFILE)
+
+
+    
+
+
+
+    clientSocket.close()
--- Server/server.py
+++ Server/server.py
@@ -1,30 +1,68 @@
 # UDPserver
 from socket import *
 import sys
+import os
 
-if (len(sys.argv) != 3):
-    print('Usage: python server.py server_port path_to_html ')
-    sys.exit()
-serverPort = int(sys.argv[1])
-filename = sys.argv[2]
+# METHOD
+def handle_request(serverSocket, directoryPath):
+    request, client_address = serverSocket.recvfrom(2048)
+    request = request.decode()
+    if not request:
+        return
+    
+    #if the request is for the html file 
+    if (request == "index.html"):
+        filePath = os.path.join(directoryPath, request)
+        if os.path.exists(filePath):
+            with open(filePath, 'rb') as f:
+                data = f.read()
+
+            chunk_size = 1024
+            for i in range(0, len(data), chunk_size):
+                chunk = data[i:i+chunk_size]
+                serverSocket.sendto(chunk, client_address)
+
+            serverSocket.sendto(EOFMSG.encode(), client_address)
+            f.close()
+        else:
+            serverSocket.sendto(ERRORMSG.encode(), client_address)
+
+    #if the request is the end of transmission message
+    elif (request == "SHITHEAD"):
+        serverSocket.close()
+        
+    #if the request is for an image
+    else:
+        response = os.path.join(directoryPath, request)
+        if os.path.exists(response):
+            serverSocket.sendto(response.encode(), client_address)
+        else:
+            serverSocket.sendto(ERRORMSG.encode(), client_address)
 
 
-print(serverPort)
-# Create UDP socket
-serverSocket = socket(AF_INET, SOCK_DGRAM)
+#MAIN
+if __name__ == '__main__':
 
-# Bind socket to local port number 12000
-serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
+    # if (len(sys.argv) != 3):
+    #     print('Usage: python3 server.py server_port path_to_html ')
+    #     sys.exit()
+    #serverPort = int(sys.argv[1])
+    #filePath = sys.argv[2]
+    directoryPath = "SampleWebPage/"
+    serverPort = 12000
+    ERRORMSG = "FILE NOT FOUND"
+    EOFMSG = "EOF"
 
-# Loop forever
-while 1:
-    # Read from UDP socket into message, getting client's
-    # address (client IP and port)
-    name, clientAddress = serverSocket.recvfrom(2048) 
-    modifiedMessage = "Hello " + name.decode()+"!"
 
-    # send string back to this client
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    # Create UDP socket
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+    # Bind socket to local port number 12000
+    serverSocket.bind(('', serverPort))
+    print("The server is ready to receive")
+
+
+    while 1:
+       handle_request(serverSocket, directoryPath)
 
     
